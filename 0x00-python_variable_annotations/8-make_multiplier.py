0d5932070350ddbb0a92bd76df310f781a9df4ea
abdulnasirym <abdulnasiry05@gmail.com>
2024-10-11 16:59:53 -0700

    using Callable from typing module

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,6 +6,8 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''return multiply'''
     def multiply(num: float) -> float:
+        '''return num(float) * multiplier(float)'''
         return num * multiplier
     return multiply
