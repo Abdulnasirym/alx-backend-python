01966c2ae761c9ddddd64b0b4062242853842ad4
abdulnasirym <abdulnasiry05@gmail.com>
2024-10-11 16:49:17 -0700

    returns tuple in an annotated function

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env pytho3
+'''type annotated function'''
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''returns a tuple'''
+    return (k, float(v ** 2))
