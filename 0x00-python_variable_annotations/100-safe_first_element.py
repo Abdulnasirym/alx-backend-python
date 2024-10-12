cc9d563b1c449266065e57a26474e1b9fb9b85ba
abdulnasirym <abdulnasiry05@gmail.com>
2024-10-11 17:27:22 -0700

    corrected duck-typed annotations

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,10 +2,10 @@
 '''Augment the following code'''
 
 
-from typing import Any, Optional, Iterable
+from typing import Any, Optional, Sequence
 
 
-def safe_first_element(lst: Iterable[Any]) -> Optional[Any]:
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     '''return first element or none'''
     if lst:
         return next(iter(lst))
