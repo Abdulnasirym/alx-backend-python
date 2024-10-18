791157ab4ac184da6c323be7e7fa0a121ca2479e
Abdulnasir Yakubu muhammad <132953212+Abdulnasirym@users.noreply.github.com>
2024-10-18 23:22:17 +0100

    measure run time

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Asynchronous function"""
+
+import asyncio
+import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Function that finds total execution time of a process"""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    stop_time = time.time()
+    return (stop_time - start_time) / n
