d6357308d96942fc46e012b7dbd4afae93c89a0f
Abdulnasirym <abdulnasiry05@gmail.com>
2024-10-18 22:34:46 +0000

    Added a function comment

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Returns sorted list of wait_times"""
     wait_times = await asyncio.gather(
         *(wait_random(max_delay) for _ in range(n))
     )
