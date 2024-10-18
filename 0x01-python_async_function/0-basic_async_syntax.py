bf7394560258d84f0f053794789a0ef9f42e5d66
Abdulnasirym <abdulnasiry05@gmail.com>
2024-10-18 21:33:54 +0000

    asynchronous fuctions

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Asynchronous coroutine"""
 
 import asyncio
@@ -6,6 +6,7 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """Asynchronous function"""
     wait_time = random.random() * max_delay
     await asyncio.sleep(wait_time)
     return wait_time
